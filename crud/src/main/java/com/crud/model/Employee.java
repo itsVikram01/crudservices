b6f8e76472ac0f0b9c8fcf71f52425ec1d19190b
itsVikram01 <vikyrajvikram95@gmail.com>
2025-01-29 03:29:35 +0530

    Initial commit

---
+++ crud/src/main/java/com/crud/model/Employee.java
@@ -0,0 +1,26 @@
+package com.crud.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "employees")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email_id")
+    private String emailId;
+}
